7b5cf8c19474635878dc5277ade6d0766fe5d5c1
Mitya Kononov <mytcat@gmail.com>
2018-05-15 13:44:23 +0300

    player page console errors fix

--- js/playerInit.js
+++ js/playerInit.js
@@ -24,7 +24,9 @@ var playerInit = function (elementsArray, containerId, urlMpd, urlMp4, useOCR, u
             console.log("everything loaded " + timeline);
             loadSubs();
             loadAudio();
-            loadOCR(useOCR);
+            if (useOCR) {
+                loadOCR(useOCR)
+            } 
             $('.vg_addon.vg_addon__list.vg_default-bg.vg_audiolist').on('mouseover', function (e) {
                 $.fn.fullpage.setAllowScrolling(false);
             });
