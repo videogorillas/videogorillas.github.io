8a3ca28f8eeaa9e2f96db5f67fb24ea217f5b985
Alex Zhukov <zhukov.alex@gmail.com>
2018-12-10 19:07:34 -0800

    diffcalc

---
+++ diffcalc/precache-manifest.c4d2480006d37fdddf3fa9b8ad369063.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34bfb0a7b032d7646c35",
+    "url": "/diffcalc/static/css/main.e2591c92.chunk.css"
+  },
+  {
+    "revision": "34bfb0a7b032d7646c35",
+    "url": "/diffcalc/static/js/main.34bfb0a7.chunk.js"
+  },
+  {
+    "revision": "864286343a9ce3d34dad",
+    "url": "/diffcalc/static/js/1.86428634.chunk.js"
+  },
+  {
+    "revision": "eaec5ba3856fcadf64f3",
+    "url": "/diffcalc/static/js/runtime~main.eaec5ba3.js"
+  },
+  {
+    "revision": "a31ffd17475951451ed001e320ee3410",
+    "url": "/diffcalc/index.html"
+  }
+];
